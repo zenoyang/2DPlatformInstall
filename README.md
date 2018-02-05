57f610a22d3fd5776246b358b9dada0e9d961797
yangzheng <yangzheng@lezhi.com>
2018-02-05 14:58:47 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Robocup Soccer Simulation 2D Server and Tools installation on Ubuntu
+
+### Setup  
+ - Clone this repository : `git clone https://github.com/yangzheng0515/2DSimulatorInstall`  
+ - Go to the directory and run the command: `./install.sh`  
+
+### Links
+ - [https://github.com/rcsoccersim](https://github.com/rcsoccersim)  
+ - [http://rctools.osdn.jp/pukiwiki/](http://rctools.osdn.jp/pukiwiki/)  
+
+### Reference  
+[http://rc-oz.osdn.jp/pukiwiki/index.php?2D/Ubuntu/16.04](http://rc-oz.osdn.jp/pukiwiki/index.php?2D/Ubuntu/16.04)  
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 安装依赖
+sudo apt-get update
+sudo apt-get install build-essential libboost-all-dev qt4-dev-tools libaudio-dev libgtk-3-dev libxt-dev flex bison libbison-dev libqt4-dev libxpm-dev libxt-dev libpng-dev libglib2.0-dev libfreetype6-dev libxrender-dev libxext-dev libfontconfig-dev libxi-dev -y
+
+# 版本配置
+RCSSSERVER="rcssserver-15.4.0"
+RCSSMONITOR="rcssmonitor-15.2.0"
+RCSSLOGPLAYER="rcsslogplayer-15.2.0"
+LIBRCSC="librcsc-4.1.0"
+SOCCERWINDOW="soccerwindow2-5.1.1"
+FEDIT="fedit2-0.0.1"
+
+# 添加安装项
+arr=($RCSSMONITOR $RCSSLOGPLAYER $LIBRCSC $SOCCERWINDOW $FEDIT)
+
+# 解压、配置、编译、安装、删除安装包
+for var in ${arr[@]}; 
+do
+	tar -zxvf $var.tar.gz && cd $var && ./configure && make && sudo make install && cd .. && rm -rf $var
+done
+
+# 添加rcsc动态库路径
+sudo echo "/usr/local/lib" >> /etc/ld.so.conf
+sudo ldconfig
